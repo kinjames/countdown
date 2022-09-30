b38e23bc3aee45c4599d1b528ca2aa40522f1b97
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-09-30 19:06:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
+const confeti = document.getElementById('conf');
 
 const newYears = "26 October 2022";
 
